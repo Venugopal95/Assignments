cc8b7e118e92a8de3548f997a8231fe97693c396
Venugopal <venu@Ashutoshs-Mac-mini.local>
2017-09-01 17:30:41 +0530

    Initial Commit

---
+++ CoreDataAssign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataAssign
+//
+//  Created by Venugopal on 01/09/17.
+//  Copyright © 2017 Venugopal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
