56bdcabc63db1b350b952c1a940be90c6cfcfda0
Venugopal <venu@Ashutoshs-Mac-mini.local>
2017-09-05 14:59:05 +0530

    Initial Commit

---
+++ NavigateAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigateAssignment
+//
+//  Created by Venugopal on 05/09/17.
+//  Copyright © 2017 Venugopal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
